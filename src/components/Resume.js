619f5b104c878c7effd776089cb8c928f9c407a9
Mohammed Sahal <118444006+mohammmmmmmmmmed@users.noreply.github.com>
2025-03-18 05:55:01 +0530

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,26 +1,18 @@
-import { Row, Col, Container } from 'react-bootstrap';
+const pdfUrl = "https://drive.google.com/file/d/1QAoAlj103PQ-Tu-AueurFWYLnIE0LfwU/preview";
 
-const Resume = () => {
-    // Direct link to the PDF file (replace with your actual PDF link)
-    const pdfUrl = "https://drive.google.com/file/d/1QAoAlj103PQ-Tu-AueurFWYLnIE0LfwU/preview";
-
-
-    return (
-        <Container fluid className="d-flex flex-column p-0">
-            <Row className="justify-content-center m-0">
-                <Col xs={12} className="d-flex flex-column align-items-center">
-                    <h1 className='glow' style={{ margin: "5vh" }}>Resume</h1>
-                    {/* Embedded PDF Viewer */}
-                    <iframe
-                        src={`https://docs.google.com/viewer?url=${encodeURIComponent(pdfUrl)}&embedded=true`}
-                        title="Resume Preview"
-                        style={{ width: "80%", height: "80vh", border: "none" }}
-                        allowFullScreen
-                    ></iframe>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
-
-export default Resume;
+return (
+    <Container fluid className="d-flex flex-column p-0">
+        <Row className="justify-content-center m-0">
+            <Col xs={12} className="d-flex flex-column align-items-center">
+                <h1 className='glow' style={{ margin: "5vh" }}>Resume</h1>
+                {/* Embedded PDF Viewer */}
+                <iframe
+                    src={pdfUrl}
+                    title="Resume Preview"
+                    style={{ width: "80%", height: "80vh", border: "none" }}
+                    allowFullScreen
+                ></iframe>
+            </Col>
+        </Row>
+    </Container>
+);
