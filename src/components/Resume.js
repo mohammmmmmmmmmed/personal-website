5a4300f96cb8331ccc9e10c7a5e12535d115319d
Mohammed Sahal <118444006+mohammmmmmmmmmed@users.noreply.github.com>
2025-03-18 05:58:25 +0530

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,26 @@
-const pdfUrl = "https://drive.google.com/file/d/1QAoAlj103PQ-Tu-AueurFWYLnIE0LfwU/preview";
+import { Row, Col, Container } from 'react-bootstrap';
 
-return (
-    <Container fluid className="d-flex flex-column p-0">
-        <Row className="justify-content-center m-0">
-            <Col xs={12} className="d-flex flex-column align-items-center">
-                <h1 className='glow' style={{ margin: "5vh" }}>Resume</h1>
-                {/* Embedded PDF Viewer */}
-                <iframe
-                    src={pdfUrl}
-                    title="Resume Preview"
-                    style={{ width: "80%", height: "80vh", border: "none" }}
-                    allowFullScreen
-                ></iframe>
-            </Col>
-        </Row>
-    </Container>
-);
+const Resume = () => {
+    // Direct link to the PDF file (replace with your actual file ID)
+    const fileId = "1QAoAlj103PQ-Tu-AueurFWYLnIE0LfwU"; // Extracted from the Google Drive URL
+    const pdfUrl = `https://drive.google.com/file/d/${fileId}/preview`;
+
+    return (
+        <Container fluid className="d-flex flex-column p-0">
+            <Row className="justify-content-center m-0">
+                <Col xs={12} className="d-flex flex-column align-items-center">
+                    <h1 className='glow' style={{ margin: "5vh" }}>Resume</h1>
+                    {/* Embedded PDF Viewer */}
+                    <iframe
+                        src={pdfUrl}
+                        title="Resume Preview"
+                        style={{ width: "80%", height: "80vh", border: "none" }}
+                        allowFullScreen
+                    ></iframe>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Resume;
